e8c2b85257fefec182cf80dc24520cce115822b8
KrotosBenjamin <kj.benjamin90@gmail.com>
2019-11-27 13:37:15 -0500

    update setup.py with email and url

--- feature_elimination_pkg/setup.py
+++ feature_elimination_pkg/setup.py
@@ -7,11 +7,11 @@ setup(
     name="feature_elimination",
     version="0.1",
     author="Kynon JM Benjamin and Apua CM Paquola",
-    author_email="...",
+    author_email="kj.benjamin90@gmail.com",
     decription="A package to preform feature elimination for random forest",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/KrotosBenjamin/erwin_paquola/tree/master/feature_elimination_pkg",
     packages=find_packages('src'),
     package_dir={"": "src"},
     classifiers=[
