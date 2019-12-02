08ad7dbb129f22d4874c17e4da802079d0c6d7db
KrotosBenjamin <kj.benjamin90@gmail.com>
2019-12-02 13:57:46 -0500

    edited license and description

--- jupy/setup.py
+++ jupy/setup.py
@@ -5,13 +5,19 @@ setup(name='jupy',
       version='0.2',
       packages=find_packages(),
       scripts=['jupy'],
-      install_requires=['jupyterlab>=1.2.3', 'argparse>=1.1'],
+      install_requires=[
+          'jupyterlab>=1.2.3',
+          'argparse>=1.1'
+      ],
       author="Kynon Benjamin & Apuã Paquola",
       author_email="kj.benjamin90@gmail.com",
-      decription="Starts a jupyter-lab session within host at directory provided.",
+      decription="A utility to launch jupyter lab remotely.",
+      long_description="A ultity that automatically finds an available port, ssh into the server, and calls jupyter lab.",
       url="https://github.com/KrotosBenjamin/erwin_paquola/tree/master/jupy",
+      license='GPLv3',
       classifiers=[
           "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: GPLv3 License",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
       ],
+      keywords='jupyter jupyterlab ssh remote',
       zip_safe=False)
