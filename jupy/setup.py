0b4af272e353d4e19138d2b52a188bc114fddb1f
KrotosBenjamin <kj.benjamin90@gmail.com>
2019-12-02 12:33:14 -0500

    add setup.py for pip installation

--- jupy/setup.py
+++ jupy/setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='jupy',
       version='0.2',
+      packages=find_packages(),
+      scripts=['jupy'],
+      install_requires=['jupyterlab>=1.2.3', 'argparse>=1.1'],
+      author="Kynon Benjamin & Apuã Paquola",
+      author_email="kj.benjamin90@gmail.com",
       decription="Starts a jupyter-lab session within host at directory provided.",
       url="https://github.com/KrotosBenjamin/erwin_paquola/tree/master/jupy",
-      author="Kynon Benjamin & Apua Paquola",
-      author_email="kj.benjamin90@gmail.com",
-      license="GPLv3",
-      packages=["jupy"],
-      python_requires=">=3.7")
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GPLv3 License",
+      ],
+      zip_safe=False)
