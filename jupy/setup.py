f26ac3db8b0d4478b7532961fb3b94645efccd29
KrotosBenjamin <jadebenj@gmail.com>
2019-12-02 11:40:50 -0500

    add package requirements

---
+++ jupy/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='jupy',
+      version='0.2',
+      decription="Starts a jupyter-lab session within host at directory provided.",
+      url="https://github.com/KrotosBenjamin/erwin_paquola/tree/master/jupy",
+      author="Kynon Benjamin & Apua Paquola",
+      author_email="kj.benjamin90@gmail.com",
+      license="GPLv3",
+      packages=["jupy"],
+      python_requires=">=3.7")
